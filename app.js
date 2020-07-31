0e7522776e556096dcebeeb35430b7b3feb37980
vcheloudiakoff <vcheloudiakoff@gmail.com>
2020-07-31 16:51:02 +0200

    Set architecture and manage 404 error on import folders

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const bp = require("body-parser");
+const path = require("path");
 
 const port = 5000;
-
 const app = express();
 
 app.use(bp.urlencoded({ extended: false }));
-
 app.use(bp.json());
 
 app.use(require("morgan")("dev"));
 
+var stylesPath = path.join(__dirname + '/styles');
+var imagesPath = path.join(__dirname + '/images');
+app.use(express.static(stylesPath));
+app.use(express.static(imagesPath));
+
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(port, () => {

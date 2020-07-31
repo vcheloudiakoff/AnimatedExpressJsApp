c42c398cf930755132deb7bd889c7d88bc8a797f
vcheloudiakoff <vcheloudiakoff@gmail.com>
2020-07-31 16:14:45 +0200

    First commit init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bp = require("body-parser");
+
+const port = 5000;
+
+const app = express();
+
+app.use(bp.urlencoded({ extended: false }));
+
+app.use(bp.json());
+
+app.use(require("morgan")("dev"));
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log(`Express server listening on port : ${port}`);
+});

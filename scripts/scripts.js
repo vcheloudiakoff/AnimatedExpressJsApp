316783660211b31c7be2d81daefec052f4de3787
vcheloudiakoff <vcheloudiakoff@gmail.com>
2020-08-03 12:23:06 +0200

    Fix animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,15 +5,16 @@ $(document).ready(function()
 });
 
 function onloadFadesAnimation() {
-    console.log("Into function onloadFadesAnimation");
-    
-    // Ce fadeIn ne fonctionne pas
-    // $('#rectangle').fadeIn(2500);
-    
-    $('#rectangle').fadeOut(2500, function() {
-        $('#rectangle').attr('id', 'rectangleBorder');
-        $('#rectangleBorder').fadeIn(1500, function(){
-            // next
+    var beginStr = $('#rectangle').text();
+
+    $('#buttonText a').text("Pret?");
+    // Premier fadeIn ne fonctionne pas
+    $('#rectangle').fadeIn(2500, function(){
+        $('#rectangle').fadeOut(2500, function() {
+            $('#buttonText a').text('Cliquez !');
+            $('#rectangle').attr('id', 'rectangleBorder');
+            $('#rectangleBorder').fadeIn(1500, function(){
+            })
         })
     });
     
